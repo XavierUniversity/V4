db61c89da1eb12639ce1fc0358f10b52f488f4af
Gregory McMullen <mcmullen.greg@gmail.com>
2016-06-10 16:13:41 -0400

    Update Gruntfile.js
    Updated the Gruntfile to include basic js linting (error checking)
    and included the compass dev and live actions.
    
    `dev` will compile nested styles for easy readability and should also
    include commentary for debugging.
    
    `live` will compile compressed styles for production environments.
    Will also include a [sourcemap](http://thesassway.com/intermediate/using-source-maps-with-sass).

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,44 @@
 module.exports = function (grunt){
 	
-	// Timing
+	// Show execution time
 	require('time-grunt')(grunt);
-	// Configuration
-	require('load-grunt-tasks')(grunt);
 	
 	grunt.initConfig({
-		pkg: grunt.file.redJSON('package.json');
-		
+		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			files:['Gruntfile.js'], // Add more files to array
+			options: {
+				reporter: require('jshint-stylish')
+			}
+		},
 		// Other tasks
+		compass: {
+			dev: {
+				options: {
+					sassDir: '_src/sass',
+					cssDir: 'css',
+					environment: 'development',
+					outputStyle: 'nested'
+					
+				}
+			},
+			live: {
+				options: {
+					sassDir: '_src/sass',
+					cssDir: 'css',
+					environment: 'production',
+					outputStyle: 'compressed',
+					sourcemap: 'true'
+				}
+			}
+		}
 	});
 	
-}
+	// Load tasks
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	
+	// Register Tasks
+	grunt.registerTask('check', ['jshint']);
+	
+};
