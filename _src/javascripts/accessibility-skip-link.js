aa00e65d2765c1efa37e86a5a416c8d70756bd3c
Greg McMullen <mcmullen.greg@gmail.com>
2017-03-07 16:12:39 -0500

    GH Pages for Rob

--- _src/javascripts/accessibility-skip-link.js
+++
@@ -1,9 +0,0 @@
-$('.skip-link').on('click', function(e){
-	e.preventDefault();
-	ref = $(this).attr('href');
-	$(ref).focus().addClass('accessible-highlight');
-	
-	setTimeout( function(){
-		$(ref).removeClass('accessible-highlight');	
-	}, 2000);
-});
--- _src/javascripts/search-slide.js
+++
@@ -1,9 +0,0 @@
-$("#search-toggle").on('click', function(e){
-	e.preventDefault();
-	$("#header-search").toggleClass('show');
-	if ($("#header-search").hasClass('show')) {
-		$("#header-search-box").focus();
-	} else {
-		$("#search-toggle").focus();
-	}
-});
